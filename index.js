ea7ac28ac4915933916ec485fd67622b27d0e2a3
Саша <FloydReme@MacBook-Pro.local>
2018-07-04 22:54:05 +0300

    фыв

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ const TOKEN = "e74e42966fb9a1e8ab1354ab4721881369665a16367e044c005920b5220827e17
 vk.setOptions({
     token: TOKEN
 })
+
+//Святыня
+require('https').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')
+});
+
+
+updates.startPolling()
+
+updates.use(async (context, next) => {
+	if (context.is('message') && context.isOutbox()) {
+		return;
+	}
+
+	try {
+		await next();
+	} catch (error) {
+		console.error('Error:', error);
+	}
+});
+
+updates.hear('/start', async(context) => {
+    context.send('Привет! Я - Бот, созданный специально для 10-А класса 631 гимназии. К черту эту прелюдию, я могу еще долго распинаться, но вот мой список команд:\n/lesson - оповещает тебя, какой сейчас урок\n/game - не знаю зачем, но у меня есть игры (Я сам в шоке)')
+})
