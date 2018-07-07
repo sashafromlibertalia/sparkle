64f7771e7313c9c799315b4dcc01395cb88209bd
Саша <FloydReme@MacBook-Pro.local>
2018-07-07 21:55:42 +0300

    asdas

--- index.js
+++ index.js
@@ -928,7 +928,7 @@ updates.hear('/дз', async(context) => {
 			minute: 'numeric'
 		  };
 		const x = Saturday.join('\n')
-		await context.send('Домашка с субботы ' + Time.toLocaleString("ru", options) + x)
+		await context.send('Домашка с субботы ' + Time.toLocaleString('ru', options, '\n') + x)
 	}
 	if(Time.getDay() === 0)
 	{
