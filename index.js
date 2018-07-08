d85fe67fe7f7919c66719d50140392ab19aa57ef
Саша <FloydReme@MacBook-Pro.local>
2018-07-08 20:29:41 +0300

    asdsa

--- index.js
+++ index.js
@@ -986,17 +986,12 @@ updates.hear('/help', async(context) => {
 
 
 
-updates.hear(/\/гдз (.+)/, async(context) => {
+updates.hear(/^\/гдз (.+)/, async(context) => {
 	const basicURL = 'https://yandex.ru/search/?text='
 	const user_input = сontext.$match[1]
 	await context.send(user_input)
 })
 
-updates.hear(/^\/reverse (.+)/i, async (context) => {
-	const text = context.$match[1];
-
-	await context.send(text);
-});
 
 const reg1 = new Array(14)
 reg1[0] = new RegExp(/мякиш/i)
