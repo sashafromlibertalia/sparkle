0499f315fbdfaa879756983580a0e9b17d423b9a
Саша <FloydReme@MacBook-Pro.local>
2018-07-14 22:29:04 +0300

    asd

--- index.js
+++ index.js
@@ -1058,15 +1058,15 @@ updates.hear(/^\/гдз (.+)/i, async (context) => {
 		Promise.all([
 			webshot(link1.href, 'images/GDZ1.png', settings, function(err) 
 		{
-			context.send('ГДЗ номер ' + 1 + ':\n' + link1.href, context.sendPhoto('images/GDZ1.png'))  
+			context.send('ГДЗ номер 1' + ':\n' + link1.href, context.sendPhoto('images/GDZ1.png'))  
 		}),
 		webshot(link2.href, 'images/GDZ2.png', settings, function(err) 
 		{
-			context.send('ГДЗ номер ' + 2 + ':\n' + link2.href, context.sendPhoto('images/GDZ2.png'))   
+			context.send('ГДЗ номер 2' + ':\n' + link2.href, context.sendPhoto('images/GDZ2.png'))   
 		}),
 		webshot(link3.href, 'images/GDZ3.png', settings, function(err) 
 		{
-			context.send('ГДЗ номер ' + 3 + ':\n' + link3.href, context.sendPhoto('images/GDZ3.png'))  
+			context.send('ГДЗ номер 3' + ':\n' + link3.href, context.sendPhoto('images/GDZ3.png'))  
 		})
 		])
 	})
