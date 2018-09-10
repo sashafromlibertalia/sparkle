e4f5effc81c9d88498e0f8eabe56d4d89a2c0d02
Саша <floydreme@yandex.ru>
2018-09-10 23:08:29 +0300

    ыв

--- index.js
+++ index.js
@@ -1375,3 +1375,30 @@ updates.hear(reg4, async(context) => {
 updates.hear(/ганж/i, async(context) => {
 	context.send('Ты рамсы попутал, пес?')
 })
+
+
+
+updates.hear(/^\/вгулаг (.+)/i, async(context) => {
+	const victim = context.$match[1]
+	if(context.senderId === 368418604)
+	{
+		if(isNaN(victim))
+		{
+			const [user] = await api.users.get({
+				user_ids: victim,
+				name_case: 'nom'
+			});
+			await context.send('ГУЛАГ тебя ждет, братишка')
+			await context.kickUser(user.id)
+		}
+		else
+		{
+			await context.send('ГУЛАГ тебя ждет, братишка')
+			await context.kickUser(victim)
+		}
+	}
+	else
+	{
+		await context.send(`Упс, ошибочка. У вас нет доступа к этой команде`)
+	}
+})
