d1ee2d5b01d5e65988be2c6fc6d55b675c51c4f8
Alex <floydreme@yandex.ru>
2019-09-06 02:27:16 +0300

    шпора 70%

--- src/shpora.js
+++ src/shpora.js
@@ -16,34 +16,42 @@ const shpora = updates.hear(/^\/шпора добавить (.+)/i, async(contex
     let category = context.$match[1]
     if (context.hasAttachments('photo')) {
       const [attached] = context.getAttachments('photo')
+      console.log(attached)
       list.push({
         text: category,
         attachment: attached.largePhoto
       })
+      await context.send('Добавлено ✅')
       console.log(list)
     } else {
       await context.send('Ошибка! Чтобы добавить шпору, прикрепите документ или фотографию к сообщению')
     }
 })
 
-const shporaGet = updates.hear(/^\/шпора (.+)/i, async(context) => {
-  let item = context.$match[1]
-  for (let i = 0; i < list.length; i++) {
-    if (item === list[i].text) {
-      upload.messagePhoto({
-        uploadUrl: list[i].attachment,
-        peer_id: config.peerID
-      })
-    }
-  }
-})
+
 
 const shporaList = updates.hear('/шпора список', async(context) => {
   let message = ''
   for (let i = 0; i < list.length; i++) {
     message += `${i+1}. ${list[i].text}\n`
   }
+  if (list.length === 0) {
+    message = 'Список пуст'
+  }
   await context.send(message)
+})
+
+
+
+const shporaGet = updates.hear(/^\/шпора (.+)/i, async(context) => {
+  let item = context.$match[1]
+  for (let i = 0; i < list.length; i++) {
+    if (item === list[i].text) {
+      context.sendPhoto(list[i].attachment)
+    } else {
+      context.send('Такой шпоры не существует :(')
+    }
+  }
 })
 
 
