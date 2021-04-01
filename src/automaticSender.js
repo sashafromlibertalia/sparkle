fd56dfefda68cf348c0177f6ec88105f54bf2650
tashareva <floydreme@yandex.ru>
2021-04-01 18:17:02 +0300

    фыв

--- src/automaticSender.js
+++ src/automaticSender.js
@@ -1,105 +1,87 @@
-const moment = require("moment");
 const DATA = require("./links");
 const BOT = require("./vk");
 
-function test(context) {
-	let cur = new Date()
-	if (cur.getHours() == 18) {
-		BOT.API.messages
-						.getConversationsById({
-							peer_ids: [context.peerId],
-						})
-						.then((res) => {
-							if (res.items[0].chat_settings.owner_id == BOT.CONFIG.ADMIN_ID || res.items[0].chat_settings.owner_id == 282987452) {
-								context.send(`я не тупой`)
-							}
-						})
-						.catch((err) => {
-							console.log(err);
-						});
-	}
-}
-
 function sendDayMessage(context) {
+	let moment = new Date();
 	setInterval(() => {
-		switch (moment().day()) {
-			// case 1:
-			// 	if (
-			// 		moment().hour() == 9 &&
-			// 		moment().minute() == 58 &&
-			// 		moment().second() == 0
-			// 	) {
-			// 		BOT.API.messages.send({
-			// 			message: `${DATA.Programming.lecture.text} ${DATA.Programming.lecture.link}`,
-			// 			chat_id: BOT.CONFIG.CHAT_ID,
-			// 			random_id: BOT.RANDOM(),
-			// 		});
-			// 	}
-			// 	if (
-			// 		moment().hour() == 11 &&
-			// 		moment().minute() == 38 &&
-			// 		moment().second() == 0
-			// 	) {
-			// 		BOT.API.messages.send({
-			// 			message: `${DATA.Programming.practice.text} ${DATA.Programming.practice.link}`,
-			// 			chat_id: BOT.CONFIG.CHAT_ID,
-			// 			random_id: BOT.RANDOM(),
-			// 		});
-			// 	}
-			// 	break;
-			// case 2:
-			// 	if (
-			// 		moment().hour() == 9 &&
-			// 		moment().minute() == 58 &&
-			// 		moment().second() == 0
-			// 	) {
-			// 		BOT.API.messages.send({
-			// 			message: `${DATA.English.text}`,
-			// 			chat_id: BOT.CONFIG.CHAT_ID,
-			// 			random_id: BOT.RANDOM(),
-			// 		});
-			// 	}
-			// 	if (
-			// 		moment().hour() == 11 &&
-			// 		moment().minute() == 38 &&
-			// 		moment().second() == 0
-			// 	) {
-			// 		BOT.API.messages.send({
-			// 			message: `${DATA.Math.lecture.text} ${DATA.Math.lecture.link}`,
-			// 			chat_id: BOT.CONFIG.CHAT_ID,
-			// 			random_id: BOT.RANDOM(),
-			// 		});
-			// 	}
-			// 	if (
-			// 		moment().hour() == 13 &&
-			// 		moment().minute() == 28 &&
-			// 		moment().second() == 0
-			// 	) {
-			// 		BOT.API.messages.send({
-			// 			message: `${DATA.DiscreteMath.practice.text} ${DATA.DiscreteMath.practice.link}`,
-			// 			chat_id: BOT.CONFIG.CHAT_ID,
-			// 			random_id: BOT.RANDOM(),
-			// 		});
-			// 	}
-			// case 3:
-			// 	// Надо четность проверять
-			// 	if (
-			// 		moment().hour() == 9 &&
-			// 		moment().minute() == 58 &&
-			// 		moment().second() == 0
-			// 	) {
-			// 		BOT.API.messages.send({
-			// 			message: `${DATA.Math.lecture.text} ${DATA.Math.lecture.link}`,
-			// 			chat_id: BOT.CONFIG.CHAT_ID,
-			// 			random_id: BOT.RANDOM(),
-			// 		});
-			// 	}
-			// 	break;
+		switch (moment.getDay()) {
+			case 1:
+				if (
+					moment.getHours() == 9 &&
+					moment.getMinutes() == 58 &&
+					moment.getSeconds() == 0
+				) {
+					BOT.API.messages.send({
+						message: `${DATA.Programming.lecture.text} ${DATA.Programming.lecture.link}`,
+						chat_id: BOT.CONFIG.CHAT_ID,
+						random_id: BOT.RANDOM(),
+					});
+				}
+				if (
+					moment.getHours() == 11 &&
+					moment.getMinutes() == 38 &&
+					moment.getSeconds() == 0
+				) {
+					BOT.API.messages.send({
+						message: `${DATA.Programming.practice.text} ${DATA.Programming.practice.link}`,
+						chat_id: BOT.CONFIG.CHAT_ID,
+						random_id: BOT.RANDOM(),
+					});
+				}
+				break;
+			case 2:
+				if (
+					moment.getHours() == 9 &&
+					moment.getMinutes() == 58 &&
+					moment.getSeconds() == 0
+				) {
+					BOT.API.messages.send({
+						message: `${DATA.English.text}`,
+						chat_id: BOT.CONFIG.CHAT_ID,
+						random_id: BOT.RANDOM(),
+					});
+				}
+				if (
+					moment.getHours() == 11 &&
+					moment.getMinutes() == 38 &&
+					moment.getSeconds() == 0
+				) {
+					BOT.API.messages.send({
+						message: `${DATA.Math.lecture.text} ${DATA.Math.lecture.link}`,
+						chat_id: BOT.CONFIG.CHAT_ID,
+						random_id: BOT.RANDOM(),
+					});
+				}
+				if (
+					moment.getHours() == 13 &&
+					moment.getMinutes() == 28 &&
+					moment.getSeconds() == 0
+				) {
+					BOT.API.messages.send({
+						message: `${DATA.DiscreteMath.practice.text} ${DATA.DiscreteMath.practice.link}`,
+						chat_id: BOT.CONFIG.CHAT_ID,
+						random_id: BOT.RANDOM(),
+					});
+				}
+			case 3:
+				// Надо четность проверять
+				if (
+					moment.getHours() == 9 &&
+					moment.getMinutes() == 58 &&
+					moment.getSeconds() == 0
+				) {
+					BOT.API.messages.send({
+						message: `${DATA.Math.lecture.text} ${DATA.Math.lecture.link}`,
+						chat_id: BOT.CONFIG.CHAT_ID,
+						random_id: BOT.RANDOM(),
+					});
+				}
+				break;
 			case 4:
 				if (
-					moment().hour() == 9 &&
-					moment().minute() == 58 &&
-					moment().second() == 0
+					moment.getHours() == 9 &&
+					moment.getMinutes() == 58 &&
+					moment.getSeconds() == 0
 				) {
 					BOT.API.messages.send({
 						message: `${DATA.Physics.lecture.text} ${DATA.Physics.lecture.link}`,
@@ -107,9 +89,9 @@ function sendDayMessage(context) {
 						random_id: BOT.RANDOM(),
 					});
 				} else if (
-					moment().hour() == 11 &&
-					moment().minute() == 38 &&
-					moment().second() == 0
+					moment.getHours() == 11 &&
+					moment.getMinutes() == 38 &&
+					moment.getSeconds() == 0
 				) {
 					BOT.API.messages
 						.send({
@@ -120,80 +102,76 @@ function sendDayMessage(context) {
 						.catch((err) => {
 							console.log(err);
 						});
-				} else if (moment().hour() == 18) {
+				} else if (moment.getHours() == 18) {
 					BOT.API.messages
-						.getConversationsById({
-							peer_ids: [context.peerId],
-						})
-						.then((res) => {
-							if (res.items[0].chat_settings.owner_id == BOT.CONFIG.ADMIN_ID || res.items[0].chat_settings.owner_id == 282987452) {
-								context.send(`я тестирую бота`)
-							}
+						.send({
+							message: `я тестирую бота`,
+							chat_id: BOT.CONFIG.CHAT_ID,
+							random_id: BOT.RANDOM(),
 						})
 						.catch((err) => {
 							console.log(err);
 						});
 				}
 				break;
-			// case 5:
-			// 	if (
-			// 		moment().hour() == 9 &&
-			// 		moment().minute() == 58 &&
-			// 		moment().second() == 0
-			// 	) {
-			// 		BOT.API.messages.send({
-			// 			message: `${DATA.English.text}`,
-			// 			chat_id: BOT.CONFIG.CHAT_ID,
-			// 			random_id: BOT.RANDOM(),
-			// 		});
-			// 	}
+			case 5:
+				if (
+					moment.getHours() == 9 &&
+					moment.getMinutes() == 58 &&
+					moment.getSeconds() == 0
+				) {
+					BOT.API.messages.send({
+						message: `${DATA.English.text}`,
+						chat_id: BOT.CONFIG.CHAT_ID,
+						random_id: BOT.RANDOM(),
+					});
+				}
 
-			// 	if (
-			// 		moment().hour() == 11 &&
-			// 		moment().minute() == 38 &&
-			// 		moment().second() == 0
-			// 	) {
-			// 		BOT.API.messages.send({
-			// 			message: `${DATA.LinearAlgebra.lecture.text} ${DATA.LinearAlgebra.lecture.link}`,
-			// 			chat_id: BOT.CONFIG.CHAT_ID,
-			// 			random_id: BOT.RANDOM(),
-			// 		});
-			// 	}
+				if (
+					moment.getHours() == 11 &&
+					moment.getMinutes() == 38 &&
+					moment.getSeconds() == 0
+				) {
+					BOT.API.messages.send({
+						message: `${DATA.LinearAlgebra.lecture.text} ${DATA.LinearAlgebra.lecture.link}`,
+						chat_id: BOT.CONFIG.CHAT_ID,
+						random_id: BOT.RANDOM(),
+					});
+				}
 
-			// 	if (
-			// 		moment().hour() == 13 &&
-			// 		moment().minute() == 28 &&
-			// 		moment().second() == 0
-			// 	) {
-			// 		BOT.API.messages.send({
-			// 			message: `${DATA.LinearAlgebra.lecture.text} ${DATA.LinearAlgebra.lecture.link}`,
-			// 			chat_id: BOT.CONFIG.CHAT_ID,
-			// 			random_id: BOT.RANDOM(),
-			// 		});
-			// 	}
-			// 	break;
-			// case 6:
-			// 	if (
-			// 		moment().hour() == 13 &&
-			// 		moment().minute() == 28 &&
-			// 		moment().second() == 0
-			// 	) {
-			// 		BOT.API.messages.send({
-			// 			message: `${DATA.Algorithms.practice.text} ${DATA.Algorithms.practice.link}`,
-			// 			chat_id: BOT.CONFIG.CHAT_ID,
-			// 			random_id: BOT.RANDOM(),
-			// 		});
-			// 	}
-			// 	break;
+				if (
+					moment.getHours() == 13 &&
+					moment.getMinutes() == 28 &&
+					moment.getSeconds() == 0
+				) {
+					BOT.API.messages.send({
+						message: `${DATA.LinearAlgebra.lecture.text} ${DATA.LinearAlgebra.lecture.link}`,
+						chat_id: BOT.CONFIG.CHAT_ID,
+						random_id: BOT.RANDOM(),
+					});
+				}
+				break;
+			case 6:
+				if (
+					moment.getHours() == 13 &&
+					moment.getMinutes() == 28 &&
+					moment.getSeconds() == 0
+				) {
+					BOT.API.messages.send({
+						message: `${DATA.Algorithms.practice.text} ${DATA.Algorithms.practice.link}`,
+						chat_id: BOT.CONFIG.CHAT_ID,
+						random_id: BOT.RANDOM(),
+					});
+				}
+				break;
 		}
-	}, 1000);
+	}, 5000);
 }
 
 module.exports = {
 	run() {
-		BOT.VK.updates.on('message', async(context) => {
-			//sendDayMessage(context);
-			test(context)
-		})
+		BOT.VK.updates.on("message", async (context) => {
+			sendDayMessage(context);
+		});
 	},
 };
