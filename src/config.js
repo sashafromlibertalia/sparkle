f572b643bd55b631f9a66ef09e8c9c2a48568deb
tashareva <floydreme@yandex.ru>
2021-05-12 21:31:02 +0300

    Версия 2.0! Переделал целиком весь код, новые команды

--- src/config.js
+++ src/config.js
@@ -1,15 +1,17 @@
-let config = {
-  TOKEN: '87322753850ec43c47043f1a45c052eee24cc123226ec82083df315f30f085d27d222e3fb0bf956fe43cd',
-  POLLING_GROUP_ID: 168462227,
-  PEER_ID: 2000000025,
-  CHAT_ID: 25,
-  PARSER_URL: 'https://github.com/sashafromlibertalia/SchoolBot/blob/master/domashka.txt',
-  NAME_GROUP: 'М3106',
-  NAME_PLACE: 'Университета ИТМО',
-  NAME_ADMIN: 'Саша',
-  NAME_ADMIN_DAT: 'Саше',
-  ADMIN_DOMAIN: 'sashafromlibertalia',
-  ADMIN_ID: [368418604, 282987452]
+/** 
+*@note
+Заполняйте данные согласно README
+*/
+const config = {
+  TOKEN: '',
+  POLLING_GROUP_ID: 0,
+  PARSER_URL: '',
+  NAME_GROUP: '',
+  NAME_PLACE: '',
+  NAME_ADMIN: '',
+  NAME_ADMIN_DAT: '',
+  ADMIN_DOMAIN: '',
+  ADMIN_ID: []
 }
 
 module.exports = config
